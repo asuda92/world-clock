5de6116a08fd84aa73116da33d4f338111c45e78
asuda92 <suda.amelia@gmail.com>
2024-09-25 09:59:30 +0200

    Added JS for single city

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector("#los-angeles .date");
+  let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss"
+  )} <small>${losAngelesTime.format("A")}</small>`;
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = document.querySelector("#tokyo .date");
+  let tokyoTimeElement = document.querySelector("#tokyo .time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format(
+    "h:mm:ss"
+  )} <small>${tokyoTime.format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
